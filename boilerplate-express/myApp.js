227626846f6a7bbe28dd5e5d02e392c6a694630b
Kevin Galeano <kevinjgaleano@gmail.com>
2024-06-26 23:15:09 +0200

    boiler step 16

--- boilerplate-express/myApp.js
+++ boilerplate-express/myApp.js
@@ -1,5 +1,23 @@
+require('dotenv').config()
+const { json } = require('body-parser');
 let express = require('express');
 let app = express();
+absolutePathIndex = __dirname + '/views/index.html'
+absolutePathPublic= __dirname + '/public'
+const salute = {
+  "message":"Hello json"
+}
+console.log("Hello World");
+app.get ('/',(req,res)=>{
+  res.sendFile(absolutePathIndex)
+})
+app.use('/public',express.static(absolutePathPublic))
+app.get('/json',(req,res)=>{
+  if (process.env.MESSAGE_STYLE=='uppercase'){
+    salute.message = salute.message.toUpperCase()
+    res.json(salute)
+  } else {res.json(salute)}
+})
 
 
 
